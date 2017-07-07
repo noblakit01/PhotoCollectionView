421bb3059cd88cf8a9d31418fd57bd83cad67e0f
Minh Luan Tran <noblakit01@gmail.com>
2017-07-06 21:04:30 -0700

    Add PhotoCell Add Simple CollectionView in Example Project

---
+++ PhotoCollectionView/PhotoCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoCollectionViewCell.swift
+//  iKidz
+//
+//  Created by luan on 7/7/17.
+//  Copyright © 2017 Sua Le. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    lazy var imageView: UIImageView! = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    lazy var moreLabel: UILabel! = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor(white: 1.0, alpha: 0.4)
+        label.isHidden = true
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(imageView)
+        leftAnchor.constraint(equalTo: imageView.leftAnchor).isActive = true
+        rightAnchor.constraint(equalTo: imageView.rightAnchor).isActive = true
+        topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        
+        addSubview(moreLabel)
+        leftAnchor.constraint(equalTo: moreLabel.leftAnchor).isActive = true
+        rightAnchor.constraint(equalTo: moreLabel.rightAnchor).isActive = true
+        topAnchor.constraint(equalTo: moreLabel.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: moreLabel.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PhotoCollectionView/ViewController.swift
+++ PhotoCollectionView/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageCollectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoCell")
+        imageCollectionView.delegate = self
+        imageCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
+extension ViewController: UICollectionViewDelegate {
+    
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        return cell
+    }
 }
 
