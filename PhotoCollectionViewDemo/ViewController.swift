8a43631bea49e3d3558e0099dfc00822f40febdd
noblakit <noblakit01@gmail.com>
2017-07-18 22:43:50 +0700

    Update PhotoCollectionViewDemo

--- PhotoCollectionViewDemo/ViewController.swift
+++ PhotoCollectionViewDemo/ViewController.swift
@@ -2,24 +2,34 @@
 //  ViewController.swift
 //  PhotoCollectionViewDemo
 //
-//  Created by luan on 7/18/17.
-//
+//  Created by Minh Luan Tran on 7/6/17.
+//  Copyright © 2017 Minh Luan Tran. All rights reserved.
 //
 
 import UIKit
+import PhotoCollectionView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageCollectionView: PhotoCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: PhotoCollectionViewDataSource {
+    func numPhotos(in photoCollectionView: PhotoCollectionView) -> Int {
+        return 10
+    }
+    
+    func photoColletionView(_ photoCollectionView: PhotoCollectionView, imageAt index: Int) -> UIImage? {
+        return UIImage(named: "image\(index + 1)")
+    }
+}
