b61a349d09333d15ae9309541dd407397230eb9d
noblakit <noblakit01@gmail.com>
2017-09-02 14:37:29 +0700

    Update frame, contentSize of SingleLayout

--- Sources/SingleLayout.swift
+++ Sources/SingleLayout.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class SingleLayout: PhotoLayoutProtocol {
+    var contentSize: CGSize = CGSize.zero
+    
     var maxPhoto: Int {
         return 1
     }
@@ -17,7 +19,18 @@ class SingleLayout: PhotoLayoutProtocol {
         guard index >= 0 && index < maxPhoto else {
             return CGRect.zero
         }
-        
-        return CGRect.zero
+        contentSize = photoCollectionView.bounds.size
+        guard let image = photoCollectionView.image(at: index) else {
+            return CGRect(origin: .zero, size: contentSize)
+        }
+        let width = photoCollectionView.bounds.width
+        var height = width * image.size.height / image.size.width
+        height = min(height, width * 1.25)
+        contentSize = CGSize(width: width, height: height)
+        return CGRect(origin: .zero, size: contentSize)
+    }
+    
+    func contentSize(of photoCollectionView: PhotoCollectionView) -> CGSize {
+        return contentSize
     }
 }
