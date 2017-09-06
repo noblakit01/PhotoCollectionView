057386674edc5ffa181d5ea7e7aeb5e90ed9b42f
noblakit <noblakit01@gmail.com>
2017-09-06 23:05:15 +0700

    Caculate contentsize for one main

--- Sources/OneMainLayout.swift
+++ Sources/OneMainLayout.swift
@@ -25,19 +25,32 @@ class OneMainLayout: PhotoLayoutProtocol {
         if index == 0 {
             var mainSize = CGSize(width: photoCollectionView.bounds.width - spacing * 2,
                                   height: photoCollectionView.bounds.height - spacing * 2)
+            contentSize = photoCollectionView.bounds.size
             isVertical = false
             if let image = photoCollectionView.image(at: index) {
                 let ratio = image.size.height / image.size.width
                 isVertical = ratio >= 1.0
                 if isVertical {
-                    mainSize.width = (photoCollectionView.bounds.width - spacing * 3) / 2
+                    mainSize.width = (photoCollectionView.bounds.width - spacing * 3) * 0.6
                     mainSize.height = mainSize.width * min(ratio, 1.25)
+                    contentSize.height = mainSize.height + spacing * 2
+                    
+                    smallItemSize.width = (photoCollectionView.bounds.width - spacing * 3) * 0.4
+                    smallItemSize.height = (mainSize.height - CGFloat(numPhoto - 2) * spacing) / CGFloat(numPhoto - 1)
                 } else {
                     mainSize.height = mainSize.width * min(ratio, 0.8)
+                    contentSize.height = mainSize.height * 1.0 / 0.6 + spacing * 3
+                    
+                    smallItemSize.width = (mainSize.width - CGFloat(numPhoto - 2) * spacing) / CGFloat(numPhoto - 1)
+                    smallItemSize.height = mainSize.height * 0.4 / 0.6
                 }
             }
             return CGRect(origin: CGPoint(x: spacing, y: spacing), size: mainSize)
         }
         return photoCollectionView.bounds
+    }
+    
+    func contentSize(of photoCollectionView: PhotoCollectionView) -> CGSize {
+        return contentSize
     }
 }
