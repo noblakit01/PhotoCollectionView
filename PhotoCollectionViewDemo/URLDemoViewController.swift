c6aedb7046fc94ab68b97ded824e01fc03761663
Luan Tran <noblakit01@gmail.com>
2017-09-20 14:09:37 +0700

    Add urls resource for testing

--- PhotoCollectionViewDemo/URLDemoViewController.swift
+++ PhotoCollectionViewDemo/URLDemoViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 import PhotoCollectionView
 
+enum UrlDemo: String {
+    case cat1 = "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"
+    case cat2 = "http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg"
+    case cat3 = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/693C/production/_95804962_p0517py6.jpg"
+    case cat4 = "http://www.petmd.com/sites/default/files/cat-lady-blog.jpg"
+    case cat5 = "http://www.lanlinglaurel.com/data/out/109/4898297-cat-picture.jpg"
+}
+
 class URLDemoViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
     var images : [[String]] = [
-        ]
+        [UrlDemo.cat1.rawValue],
+        [UrlDemo.cat1.rawValue, UrlDemo.cat2.rawValue],
+        [UrlDemo.cat1.rawValue, UrlDemo.cat2.rawValue, UrlDemo.cat3.rawValue],
+        [UrlDemo.cat1.rawValue, UrlDemo.cat2.rawValue, UrlDemo.cat3.rawValue, UrlDemo.cat4.rawValue],
+        [UrlDemo.cat1.rawValue, UrlDemo.cat2.rawValue, UrlDemo.cat3.rawValue, UrlDemo.cat4.rawValue, UrlDemo.cat5.rawValue],
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
