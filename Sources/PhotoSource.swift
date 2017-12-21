4fe8c64d880634f47221c3ce203da5554f3c37dc
Luan Tran <noblakit01@gmail.com>
2017-12-21 21:00:38 +0700

    Add PhotoSource enum

---
+++ Sources/PhotoSource.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoSource.swift
+//  PhotoCollectionView
+//
+//  Created by Minh Luan Tran on 12/21/17.
+//
+
+import UIKit
+
+enum PhotoSource {
+    case image(UIImage)
+    case url(URL)
+}
